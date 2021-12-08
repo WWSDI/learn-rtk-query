5d4fb0369d9f512c8e42a35ce5444a4df935cd67
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-12-09 10:13:26 +1300

    add: pokemonApi into store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { pokemonApi } from "./../services/pokemon";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
