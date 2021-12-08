b454929956c4dfcafdd6dfe46b43421a19e0b187
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-12-09 11:00:42 +1300

    change: App component to query for multiple pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
+import { useState } from "react";
 import "./App.css";
-import { useGetPokemonByNameQuery } from "./services/pokemon";
+import Pokemon from "./Pokemon";
+
+const pokemons = ["bulbasaur", "pikachu", "ditto", "bulbasaur"];
 
 function App() {
-  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  const [pollingInterval, setPollingInterval] = useState(0);
 
   return (
     <div className="App">
-      {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <>
-          <h3>{data.species.name}</h3>
-          <img src={data.sprites.front_shiny} alt={data.species.name} />
-        </>
-      ) : null}
+      <select
+        onChange={(change) => setPollingInterval(Number(change.target.value))}
+      >
+        <option value={0}>No Polling</option>
+        <option value={1000}>1s</option>
+        <option value={5000}>5s</option>
+      </select>
+      <div>
+        {pokemons.map((pokemon, i) => (
+          <Pokemon key={i} name={pokemon} pollingInterval={pollingInterval} />
+        ))}
+      </div>
     </div>
   );
 }
